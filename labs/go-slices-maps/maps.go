6bc24949b8dfdcfcb49aefdeb20276a2724ca938
Mauricio <maumarlam@hotmail.com>
2020-02-12 18:59:53 -0600

    Solve maps
    Signed-off-by: Mauricio <maumarlam@hotmail.com>

--- labs/go-slices-maps/maps.go
+++ labs/go-slices-maps/maps.go
@@ -1,4 +1,16 @@
 package main
-
+import (
+	"strings"
+	"golang.org/x/tour/wc"
+)
+func WordCount(s string) map[string]int {
+	m := make(map[string]int) //Creo mi mapa que luego lleno
+	x := strings.Fields(s) //Me regresa las palabras separadas por espacios 
+	for _, n := range x { //Solo me importa el valor, no el indice, para contar
+		m[n]++
+	}
+	return m
+}
 func main() {
+	wc.Test(WordCount)
 }
