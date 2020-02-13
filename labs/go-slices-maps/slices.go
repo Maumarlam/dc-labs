3bb5f0b11baacef1082e56f57a0761ccf1a61482
Mauricio <maumarlam@hotmail.com>
2020-02-12 18:41:45 -0600

    Solve slices
    Signed-off-by: Mauricio <maumarlam@hotmail.com>

--- labs/go-slices-maps/slices.go
+++ labs/go-slices-maps/slices.go
@@ -1,4 +1,23 @@
 package main
 
-func main() {
+
+import (
+	"golang.org/x/tour/pic"
+)
+
+func Pic(dx, dy int) [][]uint8 {
+	pixel := make([][]uint8, dy)
+	data := make([]uint8, dx)
+
+	for i := range pixel {		//Me va a dar el index en dy
+		for j := range data {	//Indice en dx
+			data[j] = uint8(i*j)	//Funcion x*y
+		}
+		pixel[i] = data			//Se la agrego a cada pixel en el rango
+	}
+	return pixel
 }
+func main() {
+	pic.Show(Pic)
+}
+
