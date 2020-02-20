fcab2a19a6540debb73c2ffd4b3a1cf6a63c161a
Mauricio <maumarlam@hotmail.com>
2020-02-19 21:01:15 -0600

    Solve go-functions-methods
    Signed-off-by: Mauricio <maumarlam@hotmail.com>

--- labs/go-functions-methods/geometry.go
+++ labs/go-functions-methods/geometry.go
@@ -5,27 +5,55 @@
 
 // Package geometry defines simple types for plane geometry.
 //!+point
-package geometry
+package main
 
 import "math"
+import "fmt"
+import "math/rand"
+import "os"
+import "strconv"
 
-type Point struct{ X, Y float64 }
 
-// traditional function
-func Distance(p, q Point) float64 {
-	return math.Hypot(q.X-p.X, q.Y-p.Y)
+//MAIN FUNCTION
+func main() {
+	sides := os.Args[1]
+	points, err := strconv.Atoi(sides)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	fmt.Printf("- Generating a [%d] sides figure \n", points)
+	aristas := make(Path, points)
+
+	fmt.Println("- Figure's vertices")
+	for i:=0; i<points; i++ {
+		point := Point{rand.Float64() * 100, rand.Float64() * 100}
+		fmt.Printf("    - (%f, %f) \n", point.X(), point.Y())
+		aristas[i] = point
+	}
+	fmt.Println("- Figure's perimeter")
+	fmt.Printf("    -")
+	distance := aristas.Distance()
+	fmt.Printf(" = %f \n" , distance)
 }
 
 // same thing, but as a method of the Point type
 func (p Point) Distance(q Point) float64 {
-	return math.Hypot(q.X-p.X, q.Y-p.Y)
+	return math.Hypot(q.X() - p.X(), q.Y() - p.Y())
 }
 
+type Point struct{ x, y float64 }
+
 //!-point
-
+func (p Point) X() float64 {
+	return p.x
+}
 //!+path
-
+func (p Point) Y() float64 {
+	return p.y
+}
 // A Path is a journey connecting the points with straight lines.
+
 type Path []Point
 
 // Distance returns the distance traveled along the path.
@@ -33,7 +61,13 @@ func (path Path) Distance() float64 {
 	sum := 0.0
 	for i := range path {
 		if i > 0 {
-			sum += path[i-1].Distance(path[i])
+			temp := path[i-1].Distance(path[i])
+			if i< len(path)-1 {
+				fmt.Printf(" %f +", temp)
+			}else{
+				fmt.Printf(" %f ", temp)
+			}
+			sum += temp
 		}
 	}
 	return sum
